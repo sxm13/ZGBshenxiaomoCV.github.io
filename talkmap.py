24b47abffc79b3ab6a7135e86b86d978cb468559
赵国斌 <53704568+sxm13@users.noreply.github.com>
2022-10-17 17:52:59 +0900

    Update talkmap.py

--- talkmap.py
+++ talkmap.py
@@ -1,16 +1,3 @@
-
-
-# # Leaflet cluster map of talk locations
-#
-# (c) 2016-2017 R. Stuart Geiger, released under the MIT license
-#
-# Run this from the _talks/ directory, which contains .md files of all your talks. 
-# This scrapes the location YAML field from each .md file, geolocates it with
-# geopy/Nominatim, and uses the getorg library to output data, HTML,
-# and Javascript for a standalone cluster map.
-#
-# Requires: glob, getorg, geopy
-
 import glob
 import getorg
 from geopy import Nominatim
@@ -41,7 +28,3 @@ for file in g:
 
 m = getorg.orgmap.create_map_obj()
 getorg.orgmap.output_html_cluster_map(location_dict, folder_name="../talkmap", hashed_usernames=False)
-
-
-
-
